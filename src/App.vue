5aaa1a505bcb1a01699fd25945ead8a64d1c8ecf
Igor-86 <i.campoli86@gmail.com>
2023-01-19 15:53:11 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+
+export default {
+  name: 'AppVue',
+  components: {
+
+  }
+}
+</script>
+
+<template>
+  <h1>AppVue</h1>
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss" as *;
+</style>
